2c50781fcf87f4baac3f84b112889640e779f600
Jaden <yangqipu@163.com>
2019-08-10 15:48:41 +0800

    Admin后台管理自定义

--- PythonWeb/5.1venu/typeidea_env/typeidea/Typeidea/blog/admin.py
+++ PythonWeb/5.1venu/typeidea_env/typeidea/Typeidea/blog/admin.py
@@ -3,12 +3,23 @@ from django.urls import reverse
 from django.utils.html import format_html
 
 from .models import Post, Category, Tag
-
+from .adminforms import PostAdminForm
+from django.contrib import admin
+from Typeidea.custom_site import custom_site
 
 # Register your models here.
 
+# StackedInline
+class PostInline(admin.TabularInline):
+    fields = ("title", "desc")
+    # 控制额外多几个
+    extra = 1
+    model = Post
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    # inlines = [PostInline]
     list_display = ("name", "status", "is_nav", "created_time", "owner")
     fields = ("name", "status", "is_nav")
 
@@ -27,13 +38,31 @@ class TagAdmin(admin.ModelAdmin):
         return super(TagAdmin, self).save_model(request, form, obj, change)
 
 
-@admin.register(Post)
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """自定义过滤器只展示当前用户分类"""
+    title = "分类过滤器"
+    parameter_name = "owner_category"
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list("id", "name")
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
+@admin.register(Post,site=custom_site)
 class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
     list_display = [
         "title", "category", "status", "created_time", "operator"
     ]
     list_display_links = []
-    list_filter = ["category", ]
+    exclude = ("owner",)
+    # list_filter = ["category", ]
+    list_filter = [CategoryOwnerFilter]
     search_fields = ["title", "category_name"]
     actions_on_top = True
     actions_on_bottom = True
@@ -44,7 +73,7 @@ class PostAdmin(admin.ModelAdmin):
     def operator(self, obj):
         return format_html(
             '<a href="{}">编辑</a>',
-            reverse('admin:blog_post_change', args=(obj.id,))
+            reverse('cus_admin:blog_post_change', args=(obj.id,))
         )
 
     operator.short_description = "操作"
